2e40adf1d2fb26e08ce5def04a403a668fc8b0f7
Harry Minsky <harry.minsky@gmail.com>
2025-07-11 19:29:44 -0400

    create curriculum block

--- app/components/OurCurriculum.tsx
+++ app/components/OurCurriculum.tsx
@@ -7,6 +7,111 @@ type OurCurriculumProps = {
   content: OurCurriculumQueryResult["ourCurriculum"];
 };
 
+function NumberComponent({
+  num,
+  className,
+  topClipY,
+  bottomClipY,
+  topClipX,
+  bottomClipX,
+  topClipId,
+  bottomClipId,
+}: {
+  num: number;
+  className: string;
+  topClipY: number;
+  bottomClipY: number;
+  topClipX: number;
+  bottomClipX: number;
+  topClipId: string;
+  bottomClipId: string;
+}) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className={`absolute size-[250px] font-bold ${className}`}
+    >
+      <defs>
+        <clipPath id={topClipId}>
+          <rect x={topClipX} y={topClipY} className="size-[250px]" />
+        </clipPath>
+
+        <clipPath id={bottomClipId}>
+          <rect x={bottomClipX} y={bottomClipY} className="size-[250px]" />
+        </clipPath>
+      </defs>
+
+      <text
+        x="50%"
+        y="50%"
+        dominant-baseline="middle"
+        text-anchor="middle"
+        clip-path={`url(#${topClipId})`}
+        className="fill-black stroke-none text-[250px]"
+      >
+        {num}
+      </text>
+
+      <text
+        x="50%"
+        y="50%"
+        dominant-baseline="middle"
+        text-anchor="middle"
+        clip-path={`url(#${bottomClipId})`}
+        className="fill-transparent stroke-black stroke-1 text-[250px]"
+      >
+        {num}
+      </text>
+    </svg>
+  );
+}
+
+function Number({ index }: { index: number }) {
+  switch (index % 3) {
+    case 0:
+      return (
+        <NumberComponent
+          num={index + 1}
+          className="bottom-[-125px] left-0"
+          topClipY={125}
+          bottomClipY={0}
+          topClipX={0}
+          bottomClipX={0}
+          topClipId={`clip-top-${index}`}
+          bottomClipId={`clip-bottom-${index}`}
+        />
+      );
+    case 1:
+      return (
+        <NumberComponent
+          num={index + 1}
+          className="bottom-[-150px] right-0"
+          topClipY={100}
+          bottomClipY={0}
+          topClipX={0}
+          bottomClipX={0}
+          topClipId={`clip-top-${index}`}
+          bottomClipId={`clip-bottom-${index}`}
+        />
+      );
+    case 2:
+      return (
+        <NumberComponent
+          num={index + 1}
+          className="left-0 top-[-125px]"
+          topClipY={-125}
+          bottomClipY={0}
+          topClipX={0}
+          bottomClipX={0}
+          topClipId={`clip-top-${index}`}
+          bottomClipId={`clip-bottom-${index}`}
+        />
+      );
+    default:
+      return null;
+  }
+}
+
 export default function OurCurriculum({ content }: OurCurriculumProps) {
   const { header, curriculumCards } = content || {};
 
@@ -29,8 +134,10 @@ export default function OurCurriculum({ content }: OurCurriculumProps) {
             className={`flex ${index % 2 != 0 ? "justify-end" : "justify-start"}`}
           >
             <div
-              className={`flex h-[524px] w-[1300px] justify-end gap-[80px] bg-white py-[70px] ${index % 2 != 0 ? "flex-row-reverse rounded-l-[20px]" : "flex-row rounded-r-[20px]"}`}
+              className={`relative z-10 flex h-[524px] w-[1300px] justify-end gap-[80px] bg-white py-[70px] ${index % 2 != 0 ? "flex-row-reverse rounded-l-[20px]" : "flex-row rounded-r-[20px]"} ${index % 2 == 0 ? "mt-[100px]" : ""}`}
             >
+              <Number index={index} />
+
               <div className="flex flex-col">
                 {card?.icon?.assetPath && (
                   <Image
