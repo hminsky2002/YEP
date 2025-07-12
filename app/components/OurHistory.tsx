34d8ddf53f54eac99e0f146201fd9e6187ac3c00
Harry Minsky <harry.minsky@gmail.com>
2025-07-12 02:02:49 -0400

    make our history mobile responsive

--- app/components/OurHistory.tsx
+++ app/components/OurHistory.tsx
@@ -12,19 +12,20 @@ export default function OurHistory({ content }: OurHistoryProps) {
 
   return (
     <div className="flex w-full flex-col py-[100px]">
-      <div className="mx-auto flex w-full flex-row items-center justify-center gap-[100px]">
+      <div className="mx-auto flex w-full items-center justify-center gap-[100px] lg:flex-row">
         <div className="flex flex-col items-center justify-center">
           <h1 className="pb-12 font-bold underline decoration-1 underline-offset-[20px]">
             {header}
           </h1>
 
-          <div className="w-[436px] text-left"></div>
+          <div className="hidden w-[436px] text-left lg:block"></div>
         </div>
-        <div className="flex w-[451px] flex-col"></div>
+        <div className="hidden w-[451px] flex-col lg:flex"></div>
       </div>
 
       <ol className="relative mx-auto items-start gap-[80px] sm:flex">
-        <div className="absolute top-[204px] h-px w-full bg-black"></div>
+        <div className="absolute top-[204px] hidden h-px w-full bg-black lg:block"></div>
+
         {timelineItems?.map((item, index) => (
           <li className="relative mb-6 sm:mb-0" key={index}>
             <div className="mb-[20px] flex flex-col items-center justify-center">
@@ -51,11 +52,11 @@ export default function OurHistory({ content }: OurHistoryProps) {
                 </div>
               )}
               <div
-                className={`z-10 mb-[-8px] flex size-6 shrink-0 items-center justify-center rounded-full bg-red ${
+                className={`z-10 mb-[-8px] hidden size-6 shrink-0 items-center justify-center rounded-full bg-red lg:flex ${
                   index % 2 === 0 ? "bg-red" : "bg-white"
                 }`}
               ></div>
-              <div className="mt-[50px] flex flex-col items-center justify-center text-center">
+              <div className="flex flex-col items-center justify-center text-center lg:mt-[50px]">
                 <div className="">
                   <h4 className="font-semibold">{item.header}</h4>
                   <div className="mt-[30px] max-w-[246px] font-normal">
