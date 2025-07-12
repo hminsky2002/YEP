7f7196e39acdca752bffbb179d693db37d37d431
Harry Minsky <harry.minsky@gmail.com>
2025-07-11 20:39:23 -0400

    add programming block

--- app/components/ProgrammingBlock.tsx
+++ app/components/ProgrammingBlock.tsx
@@ -11,16 +11,32 @@ export default function ProgrammingBlock({ content }: ProgrammingBlockProps) {
   const { header, programmingCards } = content || {};
 
   return (
-    <div className="flex w-full flex-col bg-yellow py-[100px]">
-      <div className="mx-auto flex w-full flex-row items-center justify-center gap-[100px]">
-        <div className="flex flex-col items-center justify-center">
-          <h1 className="font-bold underline decoration-1 underline-offset-[20px]">
-            {header}
-          </h1>
-
-          <div className="w-[436px] text-left"></div>
-        </div>
-        <div className="flex w-[451px] flex-col"></div>
+    <div className="mx-[14%] flex w-full flex-col bg-yellow">
+      <h1 className="mt-[9.8rem] underline decoration-1 underline-offset-[29px]">
+        {header}
+      </h1>
+      <div className="my-36 flex flex-row gap-[107px]">
+        {programmingCards?.map((card, index) => (
+          <div className="flex flex-col" key={index}>
+            {card?.image?.assetPath && (
+              <Image
+                src={card.image.assetPath}
+                alt={card.image.caption || ""}
+                width={1000}
+                height={1000}
+                className="drop-shadow-image-right size-[300px] object-cover"
+              />
+            )}
+            <div className="mx-auto mt-[3.4rem] w-[226px] rounded-full border border-black bg-red py-1 text-center text-[15px] font-bold text-white">
+              {card.title}
+            </div>
+            <div className="mt-14 max-w-[300px] text-center">
+              <CustomPortableText
+                value={card.description as PortableTextBlock[]}
+              />
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
